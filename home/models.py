4be41ea930fc1d6a25badbe1706b23d73cbef01c
Andrew Luchuk <andrew.luchuk@outlook.com>
2022-07-26 23:22:57 -0400

    adds initial skeleton of an editable page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.admin.panels import FieldPanel
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
     pass
+
+
+class EditablePage(Page):
+    publication_date = models.DateField("Publication date")
+    author_name = models.CharField(max_length=255)
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('author_name'),
+        FieldPanel('publication_date'),
+        FieldPanel('body'),
+    ]
