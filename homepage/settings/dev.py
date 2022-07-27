41f02b65bcc7fef1a2d964ad52b1c4bc18304ce5
Andrew Luchuk <andrew.luchuk@outlook.com>
2022-07-26 23:05:23 -0400

    creates wagtail site and install dependencies

---
+++ homepage/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-@ia#^yzpj=9d&$ed$v4(ngppv$*c4&^ruf&nv1u=98=m8^q@7-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
